6c8e8439a4013ed8fd26b35d478090873b65c971
perlancar (on notebook-dell-xps13-9315-jun2024) <perlancar@gmail.com>
2024-12-06 09:58:33 +0700

    Initial commit

---
+++ lib/App/repeat.pm
@@ -0,0 +1,22 @@
+package App::repeat;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Repeat a command a number of times
+
+=head1 SYNOPSIS
+
+See the command-line script L<repeat>.
+
+
+=head1 SEE ALSO
+
+L<App::norepeat>
+
+=cut
